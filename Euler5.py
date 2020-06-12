56758fc242f8f9a895a2552a1ec24f5483f98509
Yukesh Kumar <itn.yukesh.kumar@grabtaxi.com>
2020-06-12 08:10:29 +0530

    added sol for #5

---
+++ Euler5.py
@@ -0,0 +1,12 @@
+# num2>num1 !! 
+def gcd(num1,num2):
+    if num1==0:
+        return num2
+    return gcd(num2%num1,num1)
+
+# prd of 2 numbers =lcm*gcd
+res=10
+for i in range(res-1,1,-1):
+    res=(res*i)//gcd(res,i)
+print(res)
+
